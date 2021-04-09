6367e817688bb3aa8354e5ba285f2891edbc76a2
doomkinn-geek <doomkinn@yandex.ru>
2021-04-09 15:11:33 +0300

    Добавьте файлы проекта.

---
+++ task1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+namespace task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string strToWrite = "", fileName = "";
+            Console.Write("Введите текст: ");
+            strToWrite = Console.ReadLine();
+            Console.Write("Введите имя файла: ");
+            fileName = Console.ReadLine();
+            try
+            {
+                File.WriteAllText(fileName, strToWrite, System.Text.Encoding.UTF8);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine($"Невозможно записать текст в файл: {e.Message}");
+                return;
+            }
+            Console.WriteLine($"Файл {fileName} успешно создан");
+        }
+    }
+}
