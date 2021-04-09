48495bfa8f02b5fca86c7bbffab0a7815a775224
doomkinn-geek <doomkinn@yandex.ru>
2021-04-09 15:47:09 +0300

    task 2 done

---
+++ task2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+
+namespace task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                File.AppendAllText("startup.txt", DateTime.Now.ToString("hh:mm:ss") + '\n');
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine($"Ошибка доступа к файлу startup.txt: {e.Message}");
+            }
+        }
+    }
+}
